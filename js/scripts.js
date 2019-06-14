c85764b76e3d568bf7bf36d65e74898bc800ec7e
Lake Bramlett <lake.bramlett@gmail.com>
2019-06-14 09:14:44 -0700

    add baseline single digit beep/boop functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 //business logic//
-function runBeepBoop(num) {
-  console.log('input number: ' + num);
+function runBeepBoop(input) {
+  console.log('input number: ' + input);
 
   var rangeNumbers = [];
+  var outputNumbers = []
 
-  for (i = 0; i <= num; i++) {
+  for (i = 0; i <= input; i++) {
     rangeNumbers.push(i);
     console.log('number range: ' + rangeNumbers);
-  };
+  };//for loop to get rangeNumbers
 
+  rangeNumbers.forEach(function(i) {
+    if (i === 3) {
+      outputNumbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (i === 2) {
+      outputNumbers.push("Boop!")
+    } else if (i === 1) {
+      outputNumbers.push("Beep!")
+    } else {
+      outputNumbers.push(i);
+    };// if/else
+  });//rangeNumbers.forEach
+
+  console.log('output numbers are: ' + outputNumbers);
 
 };//function runBeepBoop
 
